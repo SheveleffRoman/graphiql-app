f6adacbc8c392b6e9ef2df6a3eb9a72595dad49b
SheveleffRoman <@>
2023-12-14 14:59:09 +0400

    feat: add footer component

--- src/pages/GraphiqlIDE/index.tsx
+++ src/pages/GraphiqlIDE/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
 
 // export interface IAppProps {}
 
@@ -8,6 +9,7 @@ export default function GraphiqlIDE() {
     <>
       <Header />
       <Link to="/">Home</Link>
+      <Footer />
     </>
   );
 }
