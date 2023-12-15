230b8325379e436172ecc4ae9b87d01782a0abc9
SheveleffRoman <@>
2023-12-15 15:24:26 +0400

    refactor: remove schema test file

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import './firebase';
-import TestPage from './сomponents/shema/test-shema';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -21,10 +20,6 @@ const router = createBrowserRouter([
   {
     path: 'register',
     element: <Register />,
-  },
-  {
-    path: 'test', // удалить в итоговой сборке
-    element: <TestPage />,
   },
 ]);
 
