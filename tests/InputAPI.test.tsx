72fd3aa3ac9d4b98715318d33c74ece92a6b32ae
SheveleffRoman <@>
2024-01-05 23:34:52 +0300

    feat: add InputAPI test

--- tests/InputAPI.test.tsx
+++ tests/InputAPI.test.tsx
@@ -8,18 +8,18 @@ import '@testing-library/jest-dom';
 
 afterEach(cleanup);
 
-const store = setupStore()
+const store = setupStore();
 
 it('InputAPI renders input and button correctly', () => {
-  const { getByText, getByDisplayValue } = render(
-      <Provider store={store}>
-        <LocalizationProvider>
-          <InputAPI />
-        </LocalizationProvider>
-      </Provider>
+  const { getByText, getByRole } = render(
+    <Provider store={store}>
+      <LocalizationProvider>
+        <InputAPI />
+      </LocalizationProvider>
+    </Provider>
   );
 
-  const inputElement = getByDisplayValue('https://rickandmortyapi.com/graphql');
+  const inputElement = getByRole('inputAPI');
   const buttonElement = getByText('Connect to api');
 
   expect(inputElement).toBeInTheDocument();
@@ -27,16 +27,14 @@ it('InputAPI renders input and button correctly', () => {
 });
 
 it('handles input change correctly', () => {
-  const { getByDisplayValue } = render(
-      <Provider store={store}>
-        <LocalizationProvider>
-          <InputAPI />
-        </LocalizationProvider>
-      </Provider>
+  const { getByRole } = render(
+    <Provider store={store}>
+      <LocalizationProvider>
+        <InputAPI />
+      </LocalizationProvider>
+    </Provider>
   );
-  const inputElement = getByDisplayValue(
-    'https://rickandmortyapi.com/graphql'
-  ) as HTMLInputElement;
+  const inputElement = getByRole('inputAPI') as HTMLInputElement;
 
   fireEvent.change(inputElement, {
     target: { value: 'https://newapi.com/graphql' },
@@ -44,31 +42,3 @@ it('handles input change correctly', () => {
 
   expect(inputElement.value).toBe('https://newapi.com/graphql');
 });
-
-// it('connects to API on button click', () => {
-//   jest.mock('../src/components/inputAPI/InputAPI', () => {
-//     return {
-//       __esModule: true,
-//       connectApi: jest.fn(),
-//     };
-//   });
-
-//   const { getByText } = render(
-//     <React.StrictMode>
-//       <Provider store={store}>
-//         <LocalizationProvider>
-//           <InputAPI />
-//         </LocalizationProvider>
-//       </Provider>
-//     </React.StrictMode>
-//   );
-
-//   const buttonElement = getByText('Connect to api');
-
-//   // fireEvent.change(inputElement, {
-//   //   target: { value: 'https://newapi.com/graphql' },
-//   // });
-//   fireEvent.click(buttonElement);
-
-//   expect(buttonElement).toHaveBeenCalled()
-// });
