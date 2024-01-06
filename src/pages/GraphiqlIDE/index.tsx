367af9c6874c30b4bf6af66144dfa8eee3e8c796
SheveleffRoman <@>
2024-01-06 21:48:08 +0300

    fix: role change

--- src/pages/GraphiqlIDE/index.tsx
+++ src/pages/GraphiqlIDE/index.tsx
@@ -113,7 +113,7 @@ export default function GraphiqlIDE() {
                   value={value}
                   theme={'dark'}
                   basicSetup={BASE_SETUP}
-                  role="queryEditorr"
+                  role="queryEditor"
                 />
               </div>
               <div className={styles.editorToolButtons}>
